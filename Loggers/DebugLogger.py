9dcd6376f11f79d56b5f6e45a1bd0248d21a98c0
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-31 12:46:15 +0530

    debuglogger

---
+++ Loggers/DebugLogger.py
@@ -0,0 +1,66 @@
+# import logging
+
+# def add(x,y):
+#     return x+y
+
+# def sub(x,y):
+#     return x-y
+
+# def mul(x,y):
+#     return x*y
+
+# def div(x,y):
+#     return x/y
+
+# def main():
+    
+#     a = 10
+#     b = 5
+#     print(f'a = {a}')
+#     print(f'b = {b}')
+    
+#     res1= add(a,b)
+#     print(f'res1 = {res1}')
+#     res2= sub(a,b)
+#     print(f'res2 = {res2}')
+#     res3= mul(a,b)
+#     print(f'res3 = {res3}')
+#     res4= div(a,b)
+#     print(f'res4 = {res4}')
+    
+# main()
+
+
+import logging
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+def main():
+    logging.basicConfig(filename='log.txt' , level=logging.DEBUG)
+    
+    a = int(input())
+    b = int(input())
+    logging.debug(f'a = {a}')
+    logging.debug(f'b = {b}')
+    
+    res1= add(a,b)
+    logging.debug(f'res1 = {res1}')
+    res2= sub(a,b)
+    logging.debug(f'res2 = {res2}')
+    res3= mul(a,b)
+    logging.debug(f'res3 = {res3}')
+    res4= div(a,b)
+    logging.debug(f'res4 = {res4}')
+    
+main()
+
