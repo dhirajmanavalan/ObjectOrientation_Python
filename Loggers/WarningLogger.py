8922cacd75b0235543df65b4dac0a2e54abbe0cb
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-31 12:50:44 +0530

    warning logger

---
+++ Loggers/WarningLogger.py
@@ -0,0 +1,15 @@
+import logging
+def validate(num):
+    if len(num) == 10:
+        print('validate mobile number..')
+    else:
+        logging.warning('Mobile number validation failed')
+        print('Invalid mobile number..')
+
+def main():
+    logging.basicConfig(filename="log.txt",level=logging.WARNING)
+    
+    num = input("Enter mobile number: \n")
+    validate(num)
+    
+main()
