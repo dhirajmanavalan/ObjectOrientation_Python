46ce4eba1e2813a8114d7384241db33203a22d92
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-03 10:44:44 +0530

    word boundary completed

--- Regular_Expression/Word Boundary.py
+++ Regular_Expression/Word Boundary.py
@@ -55,11 +55,159 @@
 # print("number of occurences: ", len(l))
 
 
-'''practise'''
+"""practise first word"""
+
+# import re
+
+# txt = "python is not java"
+# regex = r"^[a-zA-Z]+"
+# l = re.findall(regex, txt)
+# print(l)
+
+
+"""practise last word"""
+
+# import re
+
+# txt = "python is not java"
+# regex = r"[a-zA-Z]+$"
+# l = re.findall(regex, txt)
+# print(l)
+
+"""I want to match exact 4 characters"""
+# import re
+
+# txt = "python is best compare to C"
+# regex = r'\b[a-zA-Z]{4}\b'
+# match = re.search(regex,txt)
+# print(match.group())
+
+"""Email address is valid or not valid"""
+
+# import re
+
+# txt = '''
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# '''
+
+# regex = r"[a-zA-Z0-9_\-$]+@gmail.com"
+
+# match = re.findall(regex,txt)
+# print(match)
+
+"""Email address ".com , .yahoo, ..." is valid or not valid"""
+
+# import re
+
+# txt = """
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# dhi@yahoo.com
+# """
+
+# regex = r"[a-zA-Z0-9_\-$]+@[a-zA-Z]+.com"
+
+# match = re.findall(regex, txt)
+# print(match)
+
+"""create separate groups-- it gives single object"""
+
+# import re
+
+# txt = """
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# dhi@yahoo.com
+# """
+
+# regex = r"([a-zA-Z0-9_\-$]+)@([a-zA-Z]+.com)"
+
+# match = re.search(regex, txt)
+# print(match.group(0))
+# print(match.group(1))
+# print(match.group(2))
+
+
+"""create separate groups-- it give every objectss"""
+
+# import re
+
+# txt = """
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# dhi@yahoo.com
+# """
+
+# regex = r"([a-zA-Z0-9_\-$]+)@([a-zA-Z]+.com)"
+
+# itr = re.finditer(regex, txt)
+
+# for m in itr:
+#     print(m.group(0))
+#     print(m.group(1))
+#     print(m.group(2))
+
+"""i wants to change domain name"""
+
+# import re
+
+# txt = """
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# dhi@yahoo.com
+# """
+
+# regex = r"@[a-zA-Z]+.com"
+
+# s = re.sub(regex,"@rooman.com",txt)
+# print(s)
+
+"""i wants to change domain name using subn"""
+
+# import re
+
+# txt = """
+# kumardhiraj77@gmail.com
+# dhir@gmail.com
+# dhi@@gma.co
+# dhi^@gmail.com
+# dhi_kumar@gmail.com
+# dhiru-@gmail.com
+# dhi@yahoo.com
+# """
+
+# regex = r"@[a-zA-Z]+.com"
+
+# s = re.subn(regex, "@rooman.com", txt)
+# print(s)
+
+
+"""i want to separate month year date"""
 
 import re
 
-txt = "python is not java"
-regex = r"^[a-zA-Z]+"
+txt = "1992/02/22"
 
- 
+s = re.split(r"[/:-]",txt)
+print(s)
