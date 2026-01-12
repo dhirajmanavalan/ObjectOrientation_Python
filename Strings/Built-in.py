d9e8388f9e68786f9007608cd6fa31324f01c97f
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-12 12:16:03 +0530

    builtin

--- Strings/Built-in.py
+++ Strings/Built-in.py
@@ -7,30 +7,46 @@
 # s = "".join(string)
 # print(s)
 
-import re
+# import re
 
 '''Program to count number of lowercase, uppercase , specialcharacter, numbers'''
 st = "JaVaIsAlaNGuagE@098!"
 
-d_count = 0
-l_count = 0
-c_count = 0
-s_count = 0
-for i in st:
-    if re.match(r"\d",i):
-        d_count += 1
+# d_count = 0
+# l_count = 0
+# c_count = 0
+# s_count = 0
+# for i in st:
+#     if re.match(r"\d",i):
+#         d_count += 1
     
-    elif re.match(r"[a-z]",i):
-        l_count += 1
+#     elif re.match(r"[a-z]",i):
+#         l_count += 1
     
-    elif re.match(r"[A-Z]", i):
-        c_count += 1
+#     elif re.match(r"[A-Z]", i):
+#         c_count += 1
     
-    else:
-        s_count +=1
+#     else:
+#         s_count +=1
         
 
-print(d_count)
-print(l_count)
-print(c_count)
-print(s_count)
+# print(d_count)
+# print(l_count)
+# print(c_count)
+# print(s_count)
+
+
+# s = "i love python"
+
+# print(s.title())
+
+# print(s.capitalize())
+
+s = "Dhiraj 123 @ Pyhton"
+
+table = s.maketrans("aeiou","AEIOU","123@")
+print(type(table))
+print(table)
+
+str_table = s.translate(table)
+print(str_table)
